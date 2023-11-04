0871ca8745fcf8d156a5e1331776f3492d2ed7bd
fruitbox12 <dylanwong007@gmail.com>
2023-11-04 03:25:58 -0700

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  future: {
+    webpack5: false,
+  },
+  webpack: (config, {}) => {
+      config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+        };
+        return config;
+  },
+};
